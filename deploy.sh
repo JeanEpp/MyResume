22cc1730043cb597e48a73a4ac533dd0f72c6f83
JeanEpp <jeanepp99@gmail.com>
2022-10-19 00:20:04 +0200

    deploy

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,4 @@ git add dist -f
 git commit -m 'deploy'
 git push -u origin main
 git subtree push --prefix dist origin gh-pages
-git push -f git@github.com:JeanEpp/JeanEpp.github.io.git master
 cd -
